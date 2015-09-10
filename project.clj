ceb94d202a6390e5a1263a2ce52e5087acf93066
Emil Bengtsson <emil@emil0r.com>
2015-09-10 19:44:43 +0200

    up version number

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject reverie-blog "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject reverie-blog "0.2.0-SNAPSHOT"
+  :description "Blog engine for reverie/CMS"
+  :url "https://github.com/emil0r/reverie-blog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [danlentz/clj-uuid "0.1.6"]
-                 [reverie-core "0.1.0-SNAPSHOT"]])
+                 [reverie-core "0.7.0-SNAPSHOT"]])
