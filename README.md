0645e1007d874c6dad69d06ee2db849a3a25bded
Emil Bengtsson <emil@emil0r.com>
2016-07-30 20:18:31 +0200

    version 0.2.1

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 blog module for reverie
 
+```clojure
+[reverie-blog "0.2.1"]
+```
+
 ## License
 
-Copyright © 2015 Emil Bengtsson
+Copyright © 2015-2016 Emil Bengtsson
+
+--
+
+Coram Deo
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reverie-blog "0.2.1-SNAPSHOT"
+(defproject reverie-blog "0.2.1"
   :description "Blog engine for reverie/CMS"
   :url "https://github.com/emil0r/reverie-blog"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [danlentz/clj-uuid "0.1.6"]
-                 [reverie-core "0.7.0-SNAPSHOT"]])
+                 [reverie-core "0.8.0-alpha2"]])
